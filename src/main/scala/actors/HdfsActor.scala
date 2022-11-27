3a2373dedd65e8cc7d200e7bfa1b15a607d5d0d3
Abraham <abraham96miller@gmail.com>
2022-11-27 01:29:15 -0800

    query and save working for shapefiles

--- src/main/scala/actors/HdfsActor.scala
+++ src/main/scala/actors/HdfsActor.scala
@@ -12,7 +12,8 @@ import edu.ucr.cs.bdlab.davinci.{GeometricPlotter, MultilevelPlot}
 import org.apache.http.impl.cookie.BasicExpiresHandler
 import org.apache.spark.sql.SaveMode
 import utils.SparkFactory.{sparkContext, sparkSession}
-
+import scala.collection.JavaConverters._
+import java.io
 import scala.reflect.io.{Directory, File}
 
 
@@ -43,17 +44,38 @@ object HdfsActor {
           println("actors.HdfsActor: starting datasource API")
           try {
             val input = file.filesource
-            val input_df = sparkSession.read.format("geojson").load(input)
+            // GeoJSON
+//            val input_df = sparkSession.read.format("geojson").load(input)
+//            val schema = input_df.schema
+//            println(schema.size)
+//            println(schema.fields.mkString(","))
+//            println(input_df.count())
+
+            //CSV with header
+//            val input_df = SpatialCSVSource.read(sparkSession, input, Seq(SpatialCSVSource.GeometryType -> "point",
+//              "header" -> "true", SpatialCSVSource.DimensionColumns -> "x,y", "delimiter" -> " ").toMap.asJava)
+
+            //CSV with no header
+//            val input_df = SpatialCSVSource.read(sparkSession, input, Seq(SpatialCSVSource.GeometryType -> "point",
+//              SpatialCSVSource.DimensionColumns -> "1,2", "delimiter" -> " ").toMap.asJava)
+
+
+            // Shapefile
+
+
+            val input_df = sparkSession.read.format("shapefile").load(input)
+
+
+
             val schema = input_df.schema
-            println(schema.size)
-            println(schema.fields.mkString(","))
-            println(input_df.count())
+
+
             val df_outPath = "data/datasource/" + file.filename
             if (Directory(df_outPath).exists){
               File(df_outPath).deleteRecursively()
               println("actors.HdfsActor: Existing folder with same name deleted")
             }
-            input_df.write.format("geojson").mode(SaveMode.Overwrite).save(df_outPath)
+            input_df.write.format("shapefile").mode(SaveMode.Overwrite).save(df_outPath)
             println("actors.HdfsActor: Dataframes created with Datasource API")
             DataFileDAL.update_status(file.filename, filestatus = "partitioned")
           }
@@ -68,7 +90,19 @@ object HdfsActor {
           try {
             println("actors.HdfsActor: Converting file " + file.filename + " to RDD")
             val input_path = "data/datasource/" + file.filename
-            val input_df  = sparkSession.read.format("geojson").load(input_path)
+            //geojson
+//            val input_df  = sparkSession.read.format("geojson").load(input_path)
+
+            //csv
+
+//            val input_df = SpatialCSVSource.read(sparkSession, input_path, Seq(SpatialCSVSource.GeometryType -> "point",
+//              "header" -> "true", SpatialCSVSource.DimensionColumns -> "x,y", "delimiter" -> " ").toMap.asJava)
+
+
+            //shapefile
+            val input_df = sparkSession.read.format("shapefile").load(input_path)
+
+
             val input_rdd = SpatialReader.dataFrameToSpatialRDD(input_df)
             //Partition
             val features = input_rdd.spatialPartition(classOf[RSGrovePartitioner])
@@ -106,17 +140,43 @@ object HdfsActor {
           println(query)
           try {
             val input_path = "data/datasource/" + query.dataset
-            val input_df  = sparkSession.read.format("geojson").load(input_path)
-            input_df.createOrReplaceTempView(query.dataset)
+
+            //geojson
+//            val input_df  = sparkSession.read.format("geojson").load(input_path)
+
+            //csv
+//            val input_df = SpatialCSVSource.read(sparkSession, input_path, Seq(SpatialCSVSource.GeometryType -> "point",
+//              "header" -> "true", SpatialCSVSource.DimensionColumns -> "x,y", "delimiter" -> " ").toMap.asJava)
+
+            //shapefile
+            val input_df = sparkSession.read.format("shapefile").load(input_path)
+
+
             var df_outPath = "data/datasource/" + query.dataset
+            var dataset = query.dataset
             if (query.saveMode=="true") {
-              df_outPath = df_outPath + "query"
+              df_outPath = df_outPath + "_new"
+              dataset += "_new"
             }
-              if (Directory(df_outPath).exists){
-                File(df_outPath).deleteRecursively()
-                println("actors.HdfsActor: Existing folder with same name deleted")
-              }
-              sparkSession.sql(query.query).write.format("geojson").mode(SaveMode.Overwrite).save(df_outPath)
+            if (Directory(df_outPath).exists){
+              File(df_outPath).deleteRecursively()
+              println("actors.HdfsActor: Existing folder with same name deleted")
+            }
+            val output_file = DataFile(filename = dataset, filetype = "geojson", filestatus = "started", filesource = df_outPath )
+            DataFileDAL.insert(output_file)
+
+            println(input_df.schema)
+
+            input_df.createOrReplaceTempView(query.dataset)
+            val new_DF = sparkSession.sql(query.query)
+            println(new_DF.schema)
+            val output_path =  new java.io.File(df_outPath)
+
+            new_DF.write.format("shapefile").mode(SaveMode.Overwrite).save(output_path.getPath)
+
+            DataFileDAL.update_status(dataset,"partitioned")
+            println("actors.HdfsActor: Dataset saved using Datasource API")
+
           } catch {
             case e: NullPointerException =>
               println("actors.HdfsActor: Dataset does not exist :" + e.toString)
